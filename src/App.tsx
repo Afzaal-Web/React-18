5baf3cb3b987bebc615becc5ef98e2f6cf5ebf58
Muhammad Afzaal <afzaalwebsolutions@gmail.com>
2026-02-05 15:24:13 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["Lahore", "karachi", "Islamabad", "Gujrat", "Pindi"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Citites"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
